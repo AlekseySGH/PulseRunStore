7d15622b99026d6d0064bd84c91211cefe9483a7
AlekseySGH <sawlexx@gmail.com>
2024-05-25 12:15:58 +0300

    add FirstTest

--- src/test/java/OnlineStore/HomeTest.java
+++ src/test/java/OnlineStore/HomeTest.java
@@ -1,8 +1,22 @@
 package OnlineStore;
 
 import OnlineStore.runner.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class HomeTest extends BaseTest {
 
+    @Test
+    public void mainPageHeaderTest() {
 
+        final String expectedResult = "ОБИРАЙ КОМФОРТ ТА СВОБОДУ";
+
+        openBaseURL();
+
+        String actualResult = getDriver().findElement(By.xpath("//h1")).getText().toUpperCase();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+    }
 }
