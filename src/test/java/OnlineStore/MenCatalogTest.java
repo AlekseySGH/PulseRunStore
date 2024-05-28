646f5f5ea12d0d693e4eb2add11d184677269ab1
AlekseySGH <sawlexx@gmail.com>
2024-05-28 22:13:34 +0300

    added filterBySizeTest

--- src/test/java/OnlineStore/MenCatalogTest.java
+++ src/test/java/OnlineStore/MenCatalogTest.java
@@ -3,7 +3,6 @@ package OnlineStore;
 import OnlineStore.runner.BaseTest;
 import OnlineStore.utils.TestUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
