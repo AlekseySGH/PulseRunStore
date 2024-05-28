c6a76f749eb85a65d845948f883e87577550bd1e
AlekseySGH <sawlexx@gmail.com>
2024-05-28 22:13:11 +0300

    added filterBySizeTest

--- src/test/java/OnlineStore/MenCatalogTest.java
+++ src/test/java/OnlineStore/MenCatalogTest.java
@@ -67,7 +67,6 @@ public class MenCatalogTest extends BaseTest {
         return sizeList;
     }
 
-
     @DataProvider(name = "notAddedBrandProvider")
     public Object[][] notAddedBrandProvider() {
         return new Object[][]{
