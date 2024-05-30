df2f61da447bf8d9faca41df3ab619e1938db622
AlekseyS <115942572+AlekseySGH@users.noreply.github.com>
2024-05-30 16:06:44 +0300

    added presenceOfColorItemsInFilterTest (#6)

--- src/test/java/OnlineStore/MenCatalogTest.java
+++ src/test/java/OnlineStore/MenCatalogTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -19,12 +20,18 @@ public class MenCatalogTest extends BaseTest {
 
     final static By FILTER_BY_BRANDS_ITEMS = By.xpath("(//div[@class='sc-cmfnrN dwlxWf'])[1]//label");
 
+    final static By FILTER_BY_SIZE_ITEMS = By.xpath("(//div[@class='sc-cmfnrN dwlxWf'])[3]//label");
+
+    final static By FILTER_BY_COLOR_ITEMS = By.xpath("(//div[@class='sc-cmfnrN dwlxWf'])[4]//label");
+
     final static By PRODUCTS_LIST = By.xpath("//div/a[contains(@href, '/online-store-front-pulse') " +
             "and not(ancestor::div[contains(@class, 'header__inner')])]//p");
 
     final static By SHOW_ALL_BRANDS_IN_FILTER = By.xpath("(//span[text() = 'Показати все'])[1]");
 
     final static By SHOW_ALL_SIZES_IN_FILTER = By.xpath("(//span[text() = 'Показати все'])[2]");
+
+    final static By SHOW_ALL_COLOR_IN_FILTER = By.xpath("(//span[text() = 'Показати все'])[3]");
 
     private void chooseBandInCheckbox(String brandName) {
         getDriver().findElement(SHOW_ALL_BRANDS_IN_FILTER).click();
@@ -104,12 +111,12 @@ public class MenCatalogTest extends BaseTest {
                 {"43"},
                 {"44"},
                 {"42.5"},
-//                {"43.5"},
+                {"43.5"},
         };
     }
 
     @Test
-    public void presenceOfBrandItemsInFilterTest() {
+    public void presenceOfBrandsItemsInFilterTest() {
         List<String> expectedFilterItemList = List.of("Adidas", "Asics", "Converse", "Dr.Martens", "Hoka",
                 "Jordan", "Native", "New Balance", "Nike", "Puma", "Reebok", "Salomon", "Vans");
 
@@ -117,14 +124,56 @@ public class MenCatalogTest extends BaseTest {
         getWait10().until(ExpectedConditions.elementToBeClickable(MEN_CATALOG_BUTTON)).click();
         getDriver().findElement(SHOW_ALL_BRANDS_IN_FILTER).click();
 
-        List<String> filterItemList = TestUtils.getTexts(FILTER_BY_BRANDS_ITEMS, getDriver());
+        List<String> filterByBrandItemList = TestUtils.getTexts(FILTER_BY_BRANDS_ITEMS, getDriver());
 
         boolean isFilterListContainsItem;
 
         for (String expectedFilterItem : expectedFilterItemList) {
-            isFilterListContainsItem = filterItemList.contains(expectedFilterItem);
+            isFilterListContainsItem = filterByBrandItemList.contains(expectedFilterItem);
 
-            Assert.assertTrue(isFilterListContainsItem);
+            Assert.assertTrue(isFilterListContainsItem, "Brand - " + expectedFilterItem + " not found in filter");
+        }
+    }
+
+    @Ignore
+    @Test
+    public void presenceOfSizeItemsInFilterTest() {
+        List<String> expectedFilterItemList = List.of("36", "37", "38", "39", "40", "41", "42", "43", "44",
+                "45", "36.5", "37.5", "38.5", "39.5", "40.5", "41.5", "42.5", "43.5", "44.5", "45.5", "46.5", "47.5");
+
+        openBaseURL();
+        getWait10().until(ExpectedConditions.elementToBeClickable(MEN_CATALOG_BUTTON)).click();
+        getDriver().findElement(SHOW_ALL_SIZES_IN_FILTER).click();
+
+        List<String> filterBySizeItemList = TestUtils.getTexts(FILTER_BY_SIZE_ITEMS, getDriver());
+
+        boolean isFilterListContainsItem;
+
+        for (String expectedFilterItem : expectedFilterItemList) {
+            isFilterListContainsItem = filterBySizeItemList.contains(expectedFilterItem);
+
+            Assert.assertTrue(isFilterListContainsItem, "Size - " + expectedFilterItem + " not found in filter");
+        }
+    }
+
+    @Ignore
+    @Test
+    public void presenceOfColorItemsInFilterTest() {
+        List<String> expectedFilterItemList = List.of("Бежевий", "Білий", "Зелений", "Рожевий", "Синій",
+                "Сірий", "Срібний", "Фіолетовий", "Червоний", "Чорний");
+
+        openBaseURL();
+        getWait10().until(ExpectedConditions.elementToBeClickable(MEN_CATALOG_BUTTON)).click();
+        getDriver().findElement(SHOW_ALL_COLOR_IN_FILTER).click();
+
+        List<String> filterByColorItemList = TestUtils.getTexts(FILTER_BY_COLOR_ITEMS, getDriver());
+
+        boolean isFilterListContainsItem;
+
+        for (String expectedFilterItem : expectedFilterItemList) {
+            isFilterListContainsItem = filterByColorItemList.contains(expectedFilterItem);
+
+            Assert.assertTrue(isFilterListContainsItem, "Color - " + expectedFilterItem + " not found in filter");
         }
     }
 
@@ -201,6 +250,7 @@ public class MenCatalogTest extends BaseTest {
         }
     }
 
+    @Ignore
     @Test(dataProvider = "availableSizesProvider")
     public void filterBySizeTest(String sizeValue) {
 
