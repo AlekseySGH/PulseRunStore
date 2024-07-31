bd763a922f223a4029aa683fdd636d387c8cb3c3
AlekseySGH <sawlexx@gmail.com>
2024-07-31 19:09:41 +0300

    WomenCatalog is added presenceOfBrandsItemsInFilterTest is added(WomenCatalog)

---
+++ src/test/java/OnlineStore/runner/LocatorUtils.java
@@ -0,0 +1,11 @@
+package OnlineStore.runner;
+
+import org.openqa.selenium.By;
+
+public class LocatorUtils {
+
+    public static final By SHOW_ALL_BRANDS_IN_FILTER = By.xpath("(//span[text() = 'Показати все'])[1]");
+
+    public static final By FILTER_BY_BRANDS_ITEMS = By.xpath("(//div[@class='sc-fzuLxF iSOZAC'])[1]//label");
+
+}
