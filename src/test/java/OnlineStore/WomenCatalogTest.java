97ec4cc4ca6b2dfc113fe31c2e8f9beeecae66de
AlekseySGH <sawlexx@gmail.com>
2024-10-16 14:44:49 +0300

    test refactoring

--- src/test/java/OnlineStore/WomenCatalogTest.java
+++ src/test/java/OnlineStore/WomenCatalogTest.java
@@ -181,6 +181,8 @@ public class WomenCatalogTest extends BaseTest {
 
         boolean isFilteredByBrandsCorrect = TestUtils.checkFilteredBrands(randomBrandsList, getDriver(), getWait10());
 
+        getWait10().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div/ul/li/button[text() = '1']"))).click();
+
         boolean isFilteredBySizesCorrect = TestUtils.checkFilteredSize(
                 randomSizesList, TestUtils.SIZES_LIST_IN_PRODUCT_PAGE, getDriver(), getWait30());
 
